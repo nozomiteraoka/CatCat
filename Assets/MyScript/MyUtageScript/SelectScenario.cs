87c269f4081cd98ec1bf8936e7145f521e511420
nozomiteraoka <studiononnon@gmail.com>
2024-09-16 12:41:04 +0900

    カウントテスト

--- Assets/MyScript/MyUtageScript/SelectScenario.cs
+++ Assets/MyScript/MyUtageScript/SelectScenario.cs
@@ -10,7 +10,6 @@ public class SelectScenario : MonoBehaviour
     [SerializeField] public UtageUguiMainGame MainGame;
 
 
-
     private　void Start() {
         
         //育成ゲームを通過していればシーンラベルを飛ばすのを実行する
@@ -26,6 +25,56 @@ public class SelectScenario : MonoBehaviour
 
     }
 
+
+    //型指定済みの設定方法　※宴のパラメーターに加える
+    public void SaveCount(){
+
+        Engine.Param.SetParameterInt("money",GameManagerScript.money);
+        Engine.Param.SetParameterInt("hp",GameManagerScript.hp);
+        Engine.Param.SetParameterInt("heart",GameManagerScript.heart);
+        Engine.Param.SetParameterInt("day",GameManagerScript.day);
+        Engine.Param.SetParameterInt("clean",GameManagerScript.clean);
+        Engine.Param.SetParameterInt("countlimit",GameManagerScript.countlimit);
+
+    }
+
+    //パラメーターを取得する
+    public void LoadCount(){
+
+        if(ExecuteUtage.isClear == true){
+
+            //ボタンを介していたら取得しない
+            Debug.Log(GameManagerScript.hp);
+
+        }else{
+            //パラメーターの取得　※宴から取ってくるときはこちら
+            GameManagerScript.money = Engine.Param.GetParameterInt("money");
+            GameManagerScript.hp = Engine.Param.GetParameterInt("hp");
+            GameManagerScript.heart = Engine.Param.GetParameterInt("heart");
+            GameManagerScript.day = Engine.Param.GetParameterInt("day");
+            GameManagerScript.clean = Engine.Param.GetParameterInt("clean");
+            GameManagerScript.countlimit = Engine.Param.GetParameterInt("countlimit");
+
+        }
+    }
+
+
+
+
+    //セーブシーンに飛ばす
+    public void SaveTransition(){
+
+        if(ExecuteUtage.isClear == true){
+
+            MainGame.OnTapSave();
+
+
+        }else{
+            
+            Debug.Log("セーブしてません");
+
+        }
+    }
 
     // シナリオを呼び出す
     private IEnumerator Execute()
---
+++ Assets/MyScript/TrainingGameScript/GameManagerScript.cs
@@ -0,0 +1,169 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public static int money;
+    public static int hp;
+    public static int heart;
+    public static int day;
+    public static int clean = 200;
+
+
+    public TextMeshProUGUI Moneytext;
+    public TextMeshProUGUI Hptext;
+    public TextMeshProUGUI Hearttext;
+    public TextMeshProUGUI Daytext;
+    public TextMeshProUGUI Cleantext;
+
+    /*room-clean*/
+    public GameObject BG01;
+    public GameObject BG02;
+    public GameObject BG03;
+
+
+    /*回数制限*/
+    public static int countlimit = 3;
+    public TextMeshProUGUI Countlimittext;
+
+    /*回数制限により無効化したいボタン*/
+    public GameObject EatBtn;
+    public GameObject WorkBtn;
+    public GameObject CleanBtn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Moneytext.text = money.ToString();
+        Hptext.text = hp.ToString();
+        Hearttext.text = heart.ToString();
+        Daytext.text = day.ToString();
+        Cleantext.text = clean.ToString();
+
+        /*回数制限*/
+        Countlimittext.text = countlimit.ToString();
+
+
+        /*room-clean　はじめの部屋は通常です*/
+        if(clean == 200){
+            BG01.SetActive (true);
+            BG02.SetActive (false);
+            BG03.SetActive (false);
+        }
+
+        
+    }
+
+    /*食べるボタンを押すと*/
+    public void EatCount(){
+        money -= 100;
+        heart += 100;
+        clean -= 100;
+        hp += 100;
+
+        countlimit　-= 1;
+    }
+
+    /*寝るボタンを押すと*/
+    public void SleepCount(){
+        heart += 100;
+        hp += 100;
+        day += 1;
+
+        /*回数制限復活*/
+        if(countlimit < 3){
+            countlimit　= 3;
+        }
+    }
+
+    /*掃除ボタンを押すと*/
+    public void CleanCount(){
+        clean += 100;
+
+        countlimit　-= 1;
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Moneytext.text = money.ToString();
+        Hptext.text = hp.ToString();
+        Hearttext.text = heart.ToString();
+        Daytext.text = day.ToString();
+        Cleantext.text = clean.ToString();
+
+        /*回数制限*/
+        Countlimittext.text = countlimit.ToString();
+
+        /*掃除ー部屋表示*/
+        if(clean == 200){
+            BG01.SetActive (true);
+            BG02.SetActive (false);
+            BG03.SetActive (false);
+
+        }
+        if(clean < 100)
+        {
+            BG01.SetActive (false);
+            BG02.SetActive (true);
+            BG03.SetActive (false);
+        }
+        if(clean < 0)
+        {
+            BG01.SetActive (false);
+            BG02.SetActive (false);
+            BG03.SetActive (true);
+        }
+
+
+        /*もし回数制限が0になったら*/
+        if(countlimit == 0){
+
+           // ボタンを非活性に
+            EatBtn.GetComponent<Button>().interactable = false;
+            WorkBtn.GetComponent<Button>().interactable = false;
+            CleanBtn.GetComponent<Button>().interactable = false;
+        }
+        else if(countlimit > 0)
+        {
+            EatBtn.GetComponent<Button>().interactable = true;
+            WorkBtn.GetComponent<Button>().interactable = true;
+            CleanBtn.GetComponent<Button>().interactable = true;
+        }
+
+
+
+        /*もし、0以下だったら = 0になる*/
+        if(money < 0){
+            money = 0;
+        }
+
+        if(hp < 0){
+            hp = 0;
+        }
+
+        if(heart < 0){
+            heart = 0;
+        }
+
+        if(day < 0){
+            day = 0;
+        }
+
+        /*掃除ストッパ*/
+        if(clean < -100){
+            clean = -100;
+        }else if(clean > 300){
+            clean = 200;
+        }
+
+        
+    }
+}
